361e155832c328a3f84287704ef9f44362bfb006
konstantin <kostya_zhinzhikov@mail.ru>
2020-05-09 22:54:16 +0400

    Написаны заглушки для PairOfTags

--- TableOfContentsGenerator/PairOfTags.cpp
+++ TableOfContentsGenerator/PairOfTags.cpp
@@ -1,1 +1,16 @@
 #include "PairOfTags.h"
+
+using namespace std;
+
+void PairOfTags::findTagsLocationsInString(const string& str, int pos)
+{
+   
+}
+
+bool PairOfTags::findPairOfTags(const vector <string>& code)
+{
+}
+
+void PairOfTags::readContentOfTag(const vector <string>& code)
+{
+}
--- TableOfContentsGenerator/PairOfTags.h
+++ TableOfContentsGenerator/PairOfTags.h
@@ -1,5 +1,20 @@
 #pragma once
+#include "LocationInText.h"
+#include <string>
+#include <vector>
+#include <iostream>
+#include <regex>
+
 class PairOfTags
 {
-};
+public:
+    LocationInText openingTagLocation, closingTagLocation; // Местоположение тега
+    std::string name;                   // Название тега
+    std::string content;                // Содержимое тега
 
+    PairOfTags();
+
+    bool findPairOfTags(const std::vector <std::string>& code);
+    void findTagsLocationsInString(const std::string& str, int pos = 0);
+    void readContentOfTag(const std::vector <std::string>& code);
+};
