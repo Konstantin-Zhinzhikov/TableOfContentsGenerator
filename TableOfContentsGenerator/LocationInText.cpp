998e4c16b21b4e114dd3799c759358652d899daa
konstantin <kostya_zhinzhikov@mail.ru>
2020-05-09 22:50:37 +0400

    Написан класс LocationInText

--- TableOfContentsGenerator/LocationInText.cpp
+++ TableOfContentsGenerator/LocationInText.cpp
@@ -1,1 +1,35 @@
 #include "LocationInText.h"
+
+using namespace std;
+
+LocationInText::LocationInText(int chIndex, int strIndex)
+{
+    charIndex = chIndex;
+    stringIndex = strIndex;
+}
+
+void LocationInText::setLocation(int chIndex, int strIndex)
+{
+    charIndex = chIndex;
+    stringIndex = strIndex;
+}
+
+bool LocationInText::operator> (LocationInText another)
+{
+    if (this->stringIndex > another.stringIndex)
+        return true;
+    else if (this->stringIndex == another.stringIndex && this->charIndex > another.charIndex)
+        return true;
+    else
+        return false;
+}
+
+bool LocationInText::operator< (LocationInText another)
+{
+    if (this->stringIndex < another.stringIndex)
+        return true;
+    else if (this->stringIndex == another.stringIndex && this->charIndex < another.charIndex)
+        return true;
+    else
+        return false;
+}
--- TableOfContentsGenerator/LocationInText.h
+++ TableOfContentsGenerator/LocationInText.h
@@ -1,5 +1,15 @@
 #pragma once
+#include <string>
+
 class LocationInText
 {
-};
+public:
+    int charIndex;
+    int stringIndex;
 
+    LocationInText(int chIndex = -1, int strIndex = -1);
+
+    void setLocation(int chIndex, int strIndex);
+    bool operator< (LocationInText);
+    bool operator> (LocationInText);
+};
