975364bcefc0f8ea6da52c12bb7a7f48212a781e
konstantin <kostya_zhinzhikov@mail.ru>
2020-05-15 23:04:29 +0400

    Заглушки TableOfContents

--- TableOfContentsGenerator/TableOfContents.cpp
+++ TableOfContentsGenerator/TableOfContents.cpp
@@ -1,1 +1,28 @@
 #include "TableOfContents.h"
+
+using namespace std;
+
+TableOfContents::TableOfContents()
+{
+
+}
+
+void TableOfContents::findAllHeaders(vector  <string> code)
+{
+
+}
+
+void TableOfContents::generateTableOfContents(const vector <string>& code)
+{
+
+}
+
+void TableOfContents::readContentsOfHeaders(const vector <string>& code)
+{
+
+}
+
+void TableOfContents::writeTableOfContentsInFile(const string& path)
+{
+
+}
--- TableOfContentsGenerator/TableOfContents.h
+++ TableOfContentsGenerator/TableOfContents.h
@@ -1,5 +1,19 @@
 #pragma once
+#include "PairOfTags.h"
+#include <fstream>
+
 class TableOfContents
 {
+
+public:
+    TableOfContents();
+
+    std::vector <std::string> codeOfTableOfContents;
+    std::vector <PairOfTags> headers;
+
+    void generateTableOfContents(const std::vector <std::string>& code);
+    void findAllHeaders(std::vector  <std::string> code);
+    void readContentsOfHeaders(const std::vector <std::string>& code);
+    void writeTableOfContentsInFile(const std::string& path);
 };
 
