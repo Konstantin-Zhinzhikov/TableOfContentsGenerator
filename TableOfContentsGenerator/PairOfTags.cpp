e9677312c1d124f8509026c04999c5ae264e461e
konstantin <kostya_zhinzhikov@mail.ru>
2020-05-17 15:40:47 +0400

    Написана findPairOfTags

--- TableOfContentsGenerator/PairOfTags.cpp
+++ TableOfContentsGenerator/PairOfTags.cpp
@@ -14,9 +14,41 @@ void PairOfTags::findTagsLocationsInString(const string& str, int pos)
    
 }
 
-bool PairOfTags::findPairOfTags(const vector <string>& code)
+/*! Находит первое вхождение тега в коде
+ *\param [in] code - html-код*/
+bool PairOfTags::findPairOfTags(const vector <string>& code, LocationInText startOfSearch)
 {
-    return false;
+    // bool found = false;
+    bool foundOpening = false, foundClosing = false;
+
+    PairOfTags tmp(this->name);
+
+    int charPos = startOfSearch.charIndex;
+
+    // Для каждой строки кода или пока не найдено
+    for (int i = startOfSearch.stringIndex; i < (int)code.size() || !(foundOpening && foundClosing); i++)
+    {
+        tmp.findTagsLocationsInString(code[i], charPos);
+
+        if (tmp.openingTagLocation.charIndex != -1 && !foundOpening)
+        {
+            this->openingTagLocation.charIndex = tmp.openingTagLocation.charIndex;
+            this->openingTagLocation.stringIndex = i;
+            foundOpening = true;
+        }
+
+        if (tmp.closingTagLocation.charIndex != -1 && !foundClosing)
+        {
+            this->closingTagLocation.charIndex = tmp.closingTagLocation.charIndex;
+            this->closingTagLocation.stringIndex = i;
+            foundClosing = true;
+        }
+
+        charPos = 0;
+        //found = foundOpening && foundClosing;
+
+    }
+    return foundOpening && foundClosing;
 }
 
 void PairOfTags::readContentOfTag(const vector <string>& code)
--- TableOfContentsGenerator/PairOfTags.h
+++ TableOfContentsGenerator/PairOfTags.h
@@ -15,7 +15,7 @@ public:
     PairOfTags(std::string name = "", LocationInText openingTagLocation = LocationInText(),
         LocationInText closingTagLocation = LocationInText());
 
-    bool findPairOfTags(const std::vector <std::string>& code);
+    bool findPairOfTags(const std::vector <std::string>& code, LocationInText startOfSearch = LocationInText (0,0));
     void findTagsLocationsInString(const std::string& str, int pos = 0);
     void readContentOfTag(const std::vector <std::string>& code);
 };
