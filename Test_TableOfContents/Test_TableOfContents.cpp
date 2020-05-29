5f49cb8f6f8755d6b8f09bd867ab8216c0851ee2
konstantin <kostya_zhinzhikov@mail.ru>
2020-05-29 23:57:14 +0400

    Тесты для TableOfContents

--- Test_TableOfContents/Test_TableOfContents.cpp
+++ Test_TableOfContents/Test_TableOfContents.cpp
@@ -2,11 +2,13 @@
 #include "CppUnitTest.h"
 #include "..\TableOfContentsGenerator\TableOfContents.h"
 
+#define TESTS_PATH "../TESTS/TableOfContents/"
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
 
 namespace TestTableOfContents
-{	
+{
 	// Тестовое API
 	void ASSERT_tagsLocationsAreEqual(PairOfTags a, PairOfTags b)
 	{
@@ -44,12 +46,144 @@ namespace TestTableOfContents
 			Assert::AreEqual(a[i], b[i]);
 	}
 
-	TEST_CLASS(TestTableOfContents)
+	TEST_CLASS(Test_findAllHeaders)
 	{
 	public:
-		
-		TEST_METHOD(Test_GenerateTableOfContents)
+
+		TEST_METHOD(TextWithHeader)
 		{
+			// Входные данные
+			ifstream codeIn(TESTS_PATH"textWithHeaders_code.html");
+			vector <string> code;
+			string s;
+			while (getline(codeIn, s))
+				code.push_back(s);
+
+			// Ожидаемые выходные данные
+			vector <PairOfTags> expectedHeaders;
+			expectedHeaders.push_back(PairOfTags("h1", 0, 0, 0, 2));
+			expectedHeaders.push_back(PairOfTags("h2", 0, 4, 0, 6));
+			expectedHeaders.push_back(PairOfTags("h2", 0, 12, 0, 14));
+			expectedHeaders.push_back(PairOfTags("h3", 0, 16, 0, 18));
+			expectedHeaders.push_back(PairOfTags("h3", 0, 24, 0, 26));
+			expectedHeaders.push_back(PairOfTags("h2", 0, 32, 0, 34));
+
+
+			// Вызов функции
+			TableOfContents actual;
+			actual.findAllHeaders(code);
+
+			//Проверка результата
+			Assert::AreEqual(actual.headers.size(), expectedHeaders.size());
+			for (int i = 0; i < expectedHeaders.size(); i++)
+			{
+				ASSERT_tagsAreEqual(actual.headers[i], expectedHeaders[i]);
+			}
+		}
+
+		TEST_METHOD(AllHeadersOneAterAnother)
+		{
+			// Входные данные
+			ifstream codeIn(TESTS_PATH"AllHeadersOneAfterAnother_code.html");
+			vector <string> code;
+			string s;
+			while (getline(codeIn, s))
+				code.push_back(s);
+
+			// Ожидаемые выходные данные
+			vector <PairOfTags> expectedHeaders;
+			expectedHeaders.push_back(PairOfTags("h1", 0, 0, 16, 0));
+			expectedHeaders.push_back(PairOfTags("h2", 0, 2, 16, 2));
+			expectedHeaders.push_back(PairOfTags("h3", 0, 4, 16, 4));
+			expectedHeaders.push_back(PairOfTags("h4", 0, 5, 16, 5));
+			expectedHeaders.push_back(PairOfTags("h5", 0, 7, 16, 7));
+			expectedHeaders.push_back(PairOfTags("h6", 0, 9, 16, 9));
+
+			// Вызов функции
+			TableOfContents actual;
+			actual.findAllHeaders(code);
+
+			//Проверка результата
+			Assert::AreEqual(actual.headers.size(), expectedHeaders.size());
+			for (int i = 0; i < expectedHeaders.size(); i++)
+			{
+				ASSERT_tagsAreEqual(actual.headers[i], expectedHeaders[i]);
+			}
 		}
 	};
+
+	TEST_CLASS(Test_generateTableOfContents)
+	{
+	public:
+		TEST_METHOD(TextWithHeaders)
+		{
+			string s; // Переменная для ввода данных
+
+			// Входные данные
+			vector <string> code;
+			ifstream codeIn(TESTS_PATH"TextWithHeaders_code.html");
+			while (getline(codeIn, s))
+				code.push_back(s);
+
+			// Ожидаемые выходные данные
+			vector <string> expected;
+			ifstream expectedIn(TESTS_PATH "TextWithHeaders_expected.html");
+			while (getline(expectedIn, s))
+				expected.push_back(s);
+
+			// Вызов функции
+			TableOfContents contents;
+			contents.generateTableOfContents(code);
+
+			// Проверка результата
+			ASSERT_areTextsEqual(expected, contents.codeOfTableOfContents);
+		}
+
+		TEST_METHOD(AllHeadersOneAfterAnother)
+		{
+			string s; // Переменная для ввода данных
+
+			// Входные данные
+			vector <string> code;
+			ifstream codeIn(TESTS_PATH"AllHeadersOneAfterAnother_code.html");
+			while (getline(codeIn, s))
+				code.push_back(s);
+
+			// Ожидаемые выходные данные
+			vector <string> expected;
+			ifstream expectedIn(TESTS_PATH "AllHeadersOneAfterAnother_expected.html");
+			while (getline(expectedIn, s))
+				expected.push_back(s);
+
+			// Вызов функции
+			TableOfContents contents;
+			contents.generateTableOfContents(code);
+
+			// Проверка результата
+			ASSERT_areTextsEqual(expected, contents.codeOfTableOfContents);
+		}
+
+		TEST_METHOD(BadStyle)
+		{
+			string s; // Переменная для ввода данных
+
+			// Входные данные
+			vector <string> code;
+			ifstream codeIn(TESTS_PATH "BadStyle_code.html");
+			while (getline(codeIn, s))
+				code.push_back(s);
+
+			// Ожидаемые выходные данные
+			vector <string> expected;
+			ifstream expectedIn(TESTS_PATH "BadStyle_expected.html");
+			while (getline(expectedIn, s))
+				expected.push_back(s);
+
+			// Вызов функции
+			TableOfContents contents;
+			contents.generateTableOfContents(code);
+
+			// Проверка результата
+			ASSERT_areTextsEqual(expected, contents.codeOfTableOfContents);
+		}
 }
