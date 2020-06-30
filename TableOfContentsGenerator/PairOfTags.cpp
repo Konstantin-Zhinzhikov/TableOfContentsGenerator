dce14c40bbbea3496c135eee0f2cc0e6674a39bc
konstantin <kostya_zhinzhikov@mail.ru>
2020-07-01 01:00:53 +0400

    Исправлен баг, добавлена нежадность

--- TableOfContentsGenerator/PairOfTags.cpp
+++ TableOfContentsGenerator/PairOfTags.cpp
@@ -26,18 +26,18 @@ void PairOfTags::findTagsLocationsInString(const string& str, int pos)
     smatch m;
 
     string tmp1;
-    tmp1.append("<");    tmp1.append(name);    tmp1.append(".*>");
+    tmp1.append("<");    tmp1.append(name);    tmp1.append(".*?>");
     regex regexpOfOpeningTag{ tmp1 };
 
     if (regex_search(s, m, regexpOfOpeningTag))
-        openingTagLocation.charIndex = m.position();
+        openingTagLocation.charIndex = m.position() + pos;
 
     string tmp2;
-    tmp2.append("<\/");    tmp2.append(name);    tmp2.append(".*>");
+    tmp2.append("</");    tmp2.append(name);    tmp2.append(">");
     regex regexpOfClosingTag{ tmp2 };
 
     if (regex_search(s, m, regexpOfClosingTag))
-        closingTagLocation.charIndex = m.position();
+        closingTagLocation.charIndex = m.position() + pos;
 }
 
 /*! Находит первое вхождение тега в коде
